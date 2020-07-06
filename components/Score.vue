c4850fe4b49f575d3c8da0f544ef349598888793
NewGyu <NewGyu@kinoboku.net>
2020-07-06 23:37:21 +0900

    ADD: initial page

---
+++ components/Score.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="score">
+
+  </div>
+</template>
+
+<style scoped>
+.score {
+  margin: 0 auto;
+  overflow: hidden;
+  text-align: right;
+}
+</style>
---
+++ components/Stage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="stage">
+  </div>
+</template>
+
+<style scoped>
+.stage {
+  position:relative;
+  margin: 0 auto;
+  overflow: hidden;
+  background: url(/img/puyo_1bg.png);
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import {AppSettings} from 'settings/settings';
+export default Vue.extend({
+
+})
+</script>
