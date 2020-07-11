e763d925c4b3f4195d1dd00546aa56900c17af27
NewGyu <NewGyu@kinoboku.net>
2020-07-11 11:59:00 +0900

    ステージの初期値をなんとなくセット

--- components/Stage.vue
+++ components/Stage.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="stage">
+  <div class="stage" :style="styleObject">
     <Batankyu />
-    <Puyo puyo-type=3 />
+    <Puyo puyo-type="3" />
   </div>
 </template>
 
 <style scoped>
 .stage {
-  position:relative;
+  position: relative;
   margin: 0 auto;
   overflow: hidden;
   background: url(/img/puyo_1bg.png);
@@ -16,8 +16,16 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import {AppSettings} from 'settings/settings';
+import { AppSettings } from '~/settings/settings'
 export default Vue.extend({
-
+  data(): any {
+    return {
+      styleObject: {
+        width: AppSettings.puyoImgWidth * AppSettings.stageCols + 'px',
+        height: AppSettings.puyoImgHeight * AppSettings.stageRows + 'px',
+        backgroundColor: AppSettings.stageBackgroundColor,
+      },
+    }
+  },
 })
 </script>
