8f8441ec233b7c2dd79fc404346bbe5d57ba48f0
NewGyu <NewGyu@kinoboku.net>
2020-07-11 09:56:12 +0900

    ぷよをコンポーネント化

---
+++ components/Puyo.vue
@@ -0,0 +1,26 @@
+<template>
+  <img clas="puyo" :src="puyoImage">
+</template>
+
+<style scoped>
+  .puyo {
+    position: absolute;
+  }
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    puyoType: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    puyoImage(): String {
+      return `/img/puyo_${this.puyoType}.png`;
+    }
+  }
+})
+</script>
